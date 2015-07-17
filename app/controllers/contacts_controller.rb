014b22e52b59abd24d70eec260fefa0d916b632c
Mario Eichler <me@eichlersolutions.de>
2015-07-17 12:52:16 +0200

    Aded mailer and view

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,5 +1,6 @@
 class ContactsController < ApplicationController
     def new
+        # Create new, empty contact object
         @contact = Contact.new
     end
     
@@ -7,6 +8,14 @@ class ContactsController < ApplicationController
         @contact = Contact.new(contact_params)
         
         if @contact.save
+            # Prepare variables for email
+            name = params[:contact][:name]
+            email = params[:contact][:email]
+            body = params[:contact][:comments]
+            
+            # Sending Mails via File ContactMailer -> Action contact_email
+            ContactMailer.contact_email(name, email, body).deliver
+            
             flash[:success] = 'Message sent successfully.'
             redirect_to new_contact_path
         else
@@ -17,6 +26,7 @@ class ContactsController < ApplicationController
     
     private
         def contact_params
+            # Security for forms
             params.require(:contact).permit(:name, :email, :comments)
         end
 end
