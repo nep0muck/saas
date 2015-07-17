08ba0e19f035bf1b91b220655d450e03718a3637
Mario Eichler <me@eichlersolutions.de>
2015-07-17 13:23:02 +0200

    Added environmental mailsettings 3

--- config/environment.rb
+++ config/environment.rb
@@ -4,13 +4,3 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
-
-ActionMailer::Base.smtp_settings {
-    :address => 'mail.eichlersolutions.de',
-    :port => '25',
-    :authentication => :plain,
-    :user_name => 'me@eichlersolutions.de',
-    :password => '!5290Qwer',
-    :domain => 'eichlersolutions.de',
-    :enable_starttls_auto => true
-}
